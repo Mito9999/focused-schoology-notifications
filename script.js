44088c68a8dd3b7fb1a590617e73b3c5efcda1b9
Mito6211 <willmoore674@gmail.com>
2021-03-14 21:45:21 -0400

    Added feature where more notifications are automatically fetched when the clear button is pressed

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const notificationsQueryString = "#main-inner > div.item-list > ul > li > div > div > div:nth-child(1) > .notify-icon";
+const notificationsQueryString =
+  "#main-inner > div.item-list > ul > li > div > div > div:nth-child(1) > .notify-icon";
+const moreButtonQueryString =
+  "#main-inner > div.item-list > ul > li.notif-more.last > a";
 
 const removeUselessNotifications = () => {
-    document
-        .querySelectorAll(notificationsQueryString)
-        .forEach(element => {
-            if (element.classList[0] === "comment-notif") {
-                element.parentElement.parentElement.parentElement.parentElement.remove();
-            }
-        });
-    document
-        .querySelectorAll(".notif-date-header")
-        .forEach(header => header.remove());
-}
+  document.querySelectorAll(notificationsQueryString).forEach((element) => {
+    if (element.classList[0] === "comment-notif") {
+      element.parentElement.parentElement.parentElement.parentElement.remove();
+    }
+  });
+  document
+    .querySelectorAll(".notif-date-header")
+    .forEach((header) => header.remove());
+  document.querySelector(moreButtonQueryString).click();
+};
 
 const clearBtn = document.createElement("button");
 
